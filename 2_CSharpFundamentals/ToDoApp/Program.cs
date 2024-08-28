80a094f8890a72b3b4e7c9d78e02b057f4b9ee00
Vincent Okeyo <VOkeyo@oldmutual.co.ke>
2024-08-28 14:40:10 +0300

    Adds debugging section

---
+++ 2_CSharpFundamentals/ToDoApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string myName = "Vincent";
+myName = "Mr." + " " + myName + "Tom " + "Okeyo";
+
+Console.WriteLine(myName);
